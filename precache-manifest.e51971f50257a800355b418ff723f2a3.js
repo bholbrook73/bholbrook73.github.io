cac5d8b397114fa07a411d8459794f8411de85c7
Bradley Holbrook <bholbrook73@gmail.com>
2020-03-29 01:27:36 -0700

    Changed Subtitle to Servillian Technology Projects

---
+++ precache-manifest.e51971f50257a800355b418ff723f2a3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "086d4e44433e6114b25d9eef6925afdd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "526b94d6b66eb7f74902",
+    "url": "/static/css/main.a8ff9719.chunk.css"
+  },
+  {
+    "revision": "3f1c5865f51ffea1087f",
+    "url": "/static/js/2.23aadfa1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.23aadfa1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "526b94d6b66eb7f74902",
+    "url": "/static/js/main.236d8ffd.chunk.js"
+  },
+  {
+    "revision": "80524adf7a680459ccea",
+    "url": "/static/js/runtime-main.f264c7c6.js"
+  }
+]);
